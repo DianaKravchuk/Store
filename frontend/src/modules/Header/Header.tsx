7bf0d55c2f8937014124110491d1a43822bfd9da
nkovlev <nkovlev4@gmail.com>
2024-08-04 19:42:29 +0300

    header test

--- frontend/src/modules/Header/Header.tsx
+++ frontend/src/modules/Header/Header.tsx
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom";
+import { categoryLinks } from "./lib/data"
+import { HiOutlineUser } from "react-icons/hi2";
+import { IoCartOutline } from "react-icons/io5";
 
 const Header: React.FC = () => {
   return (
-    <div className="text-black font-bold text-2xl">Header</div>
+    <div className="w-[1032px] h-11 flex items-center justify-between mt-7">
+        <span className="font-Jost font-extrabold text-[#2D2D2D] leading-[18px] text-[18px] tracking-widest">SHADE.</span>
+        <div className="w-[385px] flex items-center justify-around">
+            {categoryLinks.map((link, index) => (
+                <span className="font-inter text-black" key={index}>{link.label}</span>
+            ))}
+        </div>
+        {/* Sign In and Log In buttons in future */}
+        <div className="w-24 flex items-center justify-between">
+            <Link to={"#"}>
+                <HiOutlineUser className="size-9 p-1"/>
+            </Link>
+            <Link to={"#"}>
+                <IoCartOutline className="size-9 p-1" />
+            </Link>
+        </div>
+    </div>
   )
 }
 
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import Header from "../../modules/Header"
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <div className="w-full flex flex-col items-center">
         <Header />
     </div>
   )
