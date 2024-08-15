0fddd52c5d3bb1241f1a2a6fa1bf7bcd1f51fa8b
nkovlev <nkovlev4@gmail.com>
2024-08-15 14:35:37 +0300

    Product Card rewrite

---
+++ frontend/src/components/ProductCard/types.ts
@@ -0,0 +1,8 @@
+export interface ProductCardProps {
+  id: number;
+  title: string;
+  price: number;
+  size: string;
+  priceBeforeSale?: number;
+  backgroundImage: string;
+}
--- frontend/src/modules/SaleProducts/SaleProducts.tsx
+++ frontend/src/modules/SaleProducts/SaleProducts.tsx
@@ -1,18 +1,24 @@
 import React from "react";
-import BigProductCard from "../../Ui/BigProductCard";
-import SmallProductCard from "../../Ui/SmallProductCard";
+import ProductCard from "../../components/ProductCard/ProductCard";
+import shirt from "./TemporaryCardImage/shirt.png";
 
 const SaleProducts: React.FC = () => {
   const testProducts = [
     {
       id: 1,
-      title: "Female Shirt",
+      title: "Men`s t-shirt",
       price: 25,
+      size: "small",
+      backgroundImage: shirt,
+      priceBeforeSale: 45,
     },
     {
       id: 2,
-      title: "Female Shirt",
+      title: "Men`s t-shirt",
       price: 25,
+      size: "small",
+      backgroundImage: shirt,
+      priceBeforeSale: 45,
     },
   ];
   return (
@@ -21,14 +27,24 @@ const SaleProducts: React.FC = () => {
         SALE
       </span>
       <div className="flex gap-6">
-        <BigProductCard id={2} title="Female shirt" price={25} />
+        <ProductCard
+          id={2}
+          title="Female shirt"
+          price={25}
+          size={"large"}
+          backgroundImage={shirt}
+          priceBeforeSale={45}
+        />
         <div className="flex flex-col gap-6">
           {testProducts.map((item) => (
-            <SmallProductCard
+            <ProductCard
               key={item.id}
               id={item.id}
               title={item.title}
               price={item.price}
+              size={item.size}
+              backgroundImage={item.backgroundImage}
+              priceBeforeSale={item.priceBeforeSale}
             />
           ))}
         </div>
