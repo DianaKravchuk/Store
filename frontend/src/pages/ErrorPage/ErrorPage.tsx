5267541c67882b55c8b9c6c6251531274c74ba17
nkovlev <nkovlev4@gmail.com>
2024-08-29 20:35:19 +0300

    rewrite ErrorPage

--- frontend/src/pages/ErrorPage/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage/ErrorPage.tsx
@@ -6,9 +6,26 @@ const ErrorPage: FC = () => {
   console.log(error);
 
   return (
-    <section className="pt-[150px]">
-      <h1>OOps!</h1>
-      <p>Sorry,this page does not exist.</p>
+    <section className="flex w-full h-[832px]">
+      <div className="w-[56.05%] bg-login h-[832px] bg-no-repeat bg-cover"></div>
+      <div className="w-[44%] pt-[200px] pl-[48px] flex flex-col items-center">
+        <p className="font-inter text-center text-[36px] font-black  leading-none md:text-[60px] lg:text-[70px]">
+          This page is
+        </p>
+        <p className="font-inter text-center text-[40px] font-black leading-none text-black-000 md:text-[68px] lg:text-[77px]">
+          under construction
+        </p>
+
+        <p className="mt-5 w-full font-montserrat md:w-[373px]">
+          Thank you for visiting this page and showing interest in our project.
+          We are thrilled about what&apos;s to come, and we can&apos;t wait to
+          share our progress with you!
+          <br />
+          <span className="font-normal text-brand-orange-400">
+            Please check back soon.
+          </span>
+        </p>
+      </div>
     </section>
   );
 };
