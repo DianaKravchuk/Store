b44e826979f0aa13d7e3c1acc5e67d54e5d6445b
nkovlev <nkovlev4@gmail.com>
2024-08-30 12:37:57 +0300

    order start

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -8,12 +8,14 @@ import Layout from "./layout/Layout/Layout";
 import Uikit from "@pages/Ui-kit/Ui-kit";
 import Auth from "./pages/Auth";
 import ErrorPage from "./pages/ErrorPage";
+import Order from "./pages/Order";
 
 const route = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<Layout />} errorElement={<ErrorPage />}>
       <Route path="/" element={<Home />} />
       <Route path="/ui-kit" element={<Uikit />} />
+      <Route path="/order" element={<Order />} />
       <Route path="/auth/:authPage?" element={<Auth />} />
     </Route>,
   ),
---
+++ frontend/src/pages/Order/Order.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Order: FC = () => {
+  return (
+    <div className="max-w-[1040px] h-full flex items-center justify-center overflow-hidden mt-[168px] mb-[200px]">
+      <p>Oprder</p>
+    </div>
+  );
+};
+
+export default Order;
