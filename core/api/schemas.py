98c2778812f770d8ee22dbc28cf5220921aacf15
CegRay <vasilenkoserg510@gmail.com>
2024-08-31 18:11:49 +0300

    Added Product Services & API with filters

---
+++ core/api/schemas.py
@@ -0,0 +1,20 @@
+from typing import Any, Generic, TypeVar
+
+from pydantic import Field
+from ninja import Schema
+
+from core.api.filters import PaginationOut
+
+TData = TypeVar("TData")
+TListItem = TypeVar("TListItem")
+
+
+class ListPaginationResponse(Schema, Generic[TListItem]):
+    items: list[TListItem]
+    pagination: PaginationOut
+
+
+class APIResponse(Schema, Generic[TData]):
+    data: TData | dict = Field(default_factory=dict)
+    meta: dict[str, Any] = Field(default_factory=dict)
+    errors: list[Any] = Field(default_factory=list)
