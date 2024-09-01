234bc5e1f9adcdb55c9923f5df066b1405adac9d
Taras Kazymir <kazemir.t23.23@gmail.com>
2024-09-01 14:57:40 +0300

    feat: add userSlice and integrate it into the project

--- frontend/src/redux/rootReducers.ts
+++ frontend/src/redux/rootReducers.ts
@@ -1,3 +1,6 @@
 import { combineReducers } from "@reduxjs/toolkit";
+import userSlice from "./slices/userSlice";
 
-export const rootReducer = combineReducers({});
+export const rootReducer = combineReducers({
+  user: userSlice,
+});
