adfd82b0b9665125d744cf4ddb7ca8b9acc6f64d
CegRay <vasilenkoserg510@gmail.com>
2024-09-13 14:19:14 +0300

    Added validators logic

--- core/apps/products/services/reviews.py
+++ core/apps/products/services/reviews.py
@@ -7,11 +7,19 @@ from dataclasses import dataclass
 from core.apps.customers.entities import Customer as CustomerEntity
 from core.apps.products.entities.products import Product as ProductEntity
 from core.apps.products.entities.reviews import Review as ReviewEntity
-from core.apps.products.exeptions.reviews import ReviewInvalidRating
+from core.apps.products.exeptions.reviews import (
+    ReviewInvalidRating,
+    SingleReviewError,
+)
 from core.apps.products.models.reviews import Review as ReviewModel
 
 
 class BaseReviewService(ABC):
+    @abstractmethod
+    def check_review_exists(
+        self, product: ProductEntity, customer: CustomerEntity,
+    ) -> bool: ...
+
     @abstractmethod
     def save_review(
         self,
@@ -35,6 +43,14 @@ class BaseReviewValidatorService(ABC):
 
 
 class ORMReviewService(BaseReviewService):
+    def check_review_exists(
+        self, product: ProductEntity, customer: CustomerEntity,
+    ) -> bool:
+        return ReviewModel.objects.filter(
+            product_id=product.id,
+            customer_id=customer.id,
+        ).exists()
+
     def save_review(
         self,
         customer: CustomerEntity,
@@ -42,7 +58,9 @@ class ORMReviewService(BaseReviewService):
         review: ReviewEntity,
     ) -> ReviewEntity:
         review_dto = ReviewModel.from_entity(
-            review=review, customer=customer, product=product,
+            review=review,
+            customer=customer,
+            product=product,
         )
         review_dto.save()
         return review_dto.to_entity()
@@ -60,6 +78,25 @@ class ReviewRatingValidatorService(BaseReviewValidatorService):
             raise ReviewInvalidRating(rating=review.rating)
 
 
+@dataclass
+class SingleReviewValidatorService(BaseReviewValidatorService):
+    service: BaseReviewService
+
+    def validate(
+        self,
+        customer: CustomerEntity,
+        product: ProductEntity,
+        *args,
+        **kwargs,
+    ):
+        if self.service.check_review_exists(product=product, customer=customer):  # noqa
+            raise SingleReviewError(
+                product_id=product.id,
+                customer_id=customer.id,
+            )
+
+
+# Composed Services
 @dataclass
 class ComposedReviewValidatorService(BaseReviewValidatorService):
     validators: list[BaseReviewValidatorService]
