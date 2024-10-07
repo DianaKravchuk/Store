de95c8ef61657a4d87e5c43d4e60005d76db1e4d
Taras Kazymir <kazemir.t23.23@gmail.com>
2024-10-07 11:29:47 +0300

    feat: add useWindowSize hook

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       "@pages": path.resolve(__dirname, "./src/pages"),
       "@modules": path.resolve(__dirname, "./src/modules"),
       "@assets": path.resolve(__dirname, "./src/assets"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
     },
   },
 });
