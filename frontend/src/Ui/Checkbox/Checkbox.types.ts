a51fc99556a1f9041f692b5e1972bef50a6bb207
Taras Kazymir <kazemir.t23.23@gmail.com>
2024-10-14 16:52:52 +0300

    feat: add variant options 'radio' and 'checkbox' to Checkbox component

--- frontend/src/Ui/Checkbox/Checkbox.types.ts
+++ frontend/src/Ui/Checkbox/Checkbox.types.ts
@@ -2,4 +2,5 @@ import { ComponentPropsWithoutRef } from "react";
 
 export interface CheckboxProps extends ComponentPropsWithoutRef<"input"> {
   label: string;
+  variant?: "radio" | "checkbox";
 }
