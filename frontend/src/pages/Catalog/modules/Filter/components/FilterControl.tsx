b4cf4aef773f538037a8e29d4bec8f8c5c52515e
Taras Kazymir <kazemir.t23.23@gmail.com>
2024-11-25 19:47:31 +0200

    feat: add FilterControl components

---
+++ frontend/src/pages/Catalog/modules/Filter/components/FilterControl.tsx
@@ -0,0 +1,28 @@
+import Button from "@/Ui/Button/Button";
+import { FC } from "react";
+
+export interface FilterControlProps {
+  onShow: () => void;
+  onRemove: () => void;
+}
+
+const FilterControl: FC<FilterControlProps> = ({ onRemove, onShow }) => {
+  return (
+    <div className="w-full max-m-[390px] flex flex-col gap-6">
+      <Button
+        size="full"
+        variant="filled"
+        text="Show results"
+        onClick={onShow}
+      />
+      <Button
+        size="full"
+        variant="outlined"
+        text="Remove filter"
+        onClick={onRemove}
+      />
+    </div>
+  );
+};
+
+export default FilterControl;
