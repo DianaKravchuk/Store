67ff573a1d499a0aa288f6bdff5b1360027e2ddb
Taras Kazymir <kazemir.t23.23@gmail.com>
2024-11-27 15:37:08 +0200

    refactor: update Categories component

--- frontend/src/pages/Catalog/modules/Categories/Categories.tsx
+++ frontend/src/pages/Catalog/modules/Categories/Categories.tsx
@@ -1,28 +1,19 @@
-import { FC } from "react";
+import { type FC } from "react";
+import { type CategoriesProps } from "./types";
+
 import { CATALOG_CATEGORIES } from "../../data";
-import { ButtonLink } from "@/Ui/ButtonLink/ButtonLink";
-import { CATALOG_CATEGORY } from "../../types";
-export interface CategoriesProps {
-  page: CATALOG_CATEGORY;
-}
+import CategoriesItem from "./CategoriesItem";
+
 const Categories: FC<CategoriesProps> = ({ page }) => {
   return (
     <section className="flex flex-col gap-6">
       <h2 className="text-2xl uppercase font-inter font-bold leading-[46px]">
         Show all for {page}
       </h2>
-      <ul className="flex flex-wrap">
+      <ul className="flex flex-wrap gap-x-1 gap-y-2">
         {CATALOG_CATEGORIES[page] &&
-          CATALOG_CATEGORIES[page].map(({ id, name, link }) => (
-            <li key={id} className="w-fit">
-              <ButtonLink
-                size="small"
-                variant="text"
-                text={name}
-                url={link}
-                textPosition="start"
-              />
-            </li>
+          CATALOG_CATEGORIES[page].map(({ id, name, isActive }) => (
+            <CategoriesItem key={id} id={id} name={name} isActive={isActive} />
           ))}
       </ul>
     </section>
