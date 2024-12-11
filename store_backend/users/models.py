32792fc3cb76458ff72ae2f4007796ea80510387
alex <a8696tk@gmail.com>
2024-12-11 17:02:44 +0200

    added user model

--- store_backend/users/models.py
+++ store_backend/users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from users.enums import Role
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=50, unique=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    role = models.CharField(
+        max_length=50,
+        choices=[(role.value, role.value) for role in Role],
+        default=Role.User.value
+    )
+    email = models.EmailField()
+    password = models.CharField(max_length=120)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"User: {self.username}"
