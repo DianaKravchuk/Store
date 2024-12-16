445150b93f7adbfe57308d061cf8aa33ae3828a4
alex <a8696tk@gmail.com>
2024-12-16 13:20:49 +0200

    Added .gitignpre file and change user model

--- store_backend/users/models.py
+++ store_backend/users/models.py
@@ -4,17 +4,19 @@ from users.enums import Role
 
 
 class User(AbstractUser):
-    username = models.CharField(max_length=50, unique=True)
-    last_name = models.CharField(max_length=50, blank=True, null=True)
-    first_name = models.CharField(max_length=50, blank=True, null=True)
     role = models.CharField(
         max_length=50,
         choices=[(role.value, role.value) for role in Role],
         default=Role.User.value
     )
-    email = models.EmailField()
-    password = models.CharField(max_length=120)
     is_active = models.BooleanField(default=False)
+
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['email']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
     def __str__(self):
         return f"User: {self.username}"
